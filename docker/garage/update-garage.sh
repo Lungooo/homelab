d5a735dac8e8ea3e0c2faae92e5086af3f026b1c
Sophie Langer <sophie@lungoo.dev>
2026-07-28 14:08:06 +0200

    fix(docker-updater): Only run when file changed

--- docker/garage/update-garage.sh
+++ docker/garage/update-garage.sh
@@ -2,23 +2,31 @@
 
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-COMPOSE_URL="https://raw.githubusercontent.com/Lungooo/homelab/refs/heads/main/docker/garage/compose.yaml"
+# REST API endpoint for the specific file contents
+API_URL="https://api.github.com/repos/Lungooo/homelab/contents/docker/garage/compose.yaml?ref=main"
 WORK_DIR="/volume1/docker/garage"
 
 cd "$WORK_DIR" || { echo "Failed to change directory to $WORK_DIR"; exit 1; }
 
-HTTP_STATUS=$(curl -s -L --etag-compare .etag --etag-save .etag -w "%{http_code}" -o compose.yaml "$COMPOSE_URL")
+# Download raw file content directly using GitHub API while passing stored ETag
+HTTP_STATUS=$(curl -s -L \
+  -H "Accept: application/vnd.github.raw+json" \
+  -H "User-Agent: Synology-Garage-Updater" \
+  --etag-compare .etag \
+  --etag-save .etag \
+  -w "%{http_code}" \
+  -o compose.yaml \
+  "$API_URL")
 
 if [ "$HTTP_STATUS" -eq 200 ]; then
-  echo "Update found: The file was downloaded successfully."
+  echo "Update found: File changed on GitHub. Downloaded new version."
   docker compose down || { echo "Failed to stop Docker containers"; exit 1; }
   sleep 2
   docker compose pull || { echo "Failed to pull Docker images"; exit 1; }
-  docker compose up -d || { echo "Failed to start Docker containers";  exit 1; }
+  docker compose up -d || { echo "Failed to start Docker containers"; exit 1; }
 
-    
 elif [ "$HTTP_STATUS" -eq 304 ]; then
-  echo "No changes: The file matches the remote version."
+  echo "No changes: The remote file content matches local ETag."
   exit 0
 
 else
